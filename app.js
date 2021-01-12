a54195f4524e1395ce3d468debddf61d2c1430ff
Khorf Zakaria <gz_khorf@esi.dz>
2021-01-12 22:10:38 +0100

    Finished Project it has been a blast !

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { connectDB } = require('./config/db')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
-
+const methodOverride = require('method-override')
 // Load config 
 dotenv.config({ path: './config/config.env' })
 
@@ -19,20 +19,36 @@ connectDB()
 
 const app = express()
 
+//Body parser
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+
+
+//Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
+
+
+
+
 //Logging 
 if (process.env.NODE_ENV === 'developement') {
     app.use(morgan('dev'))
 }
 
 //HandleBars Helpers
-const { formatDate , stripTags , truncate} = require('./helpers/hbs')
+const { formatDate , stripTags , truncate ,editIcon, select } = require('./helpers/hbs')
 
 
 
 //  Handlebars
-app.engine('.hbs', exhbs({helpers:{formatDate , stripTags,truncate
+app.engine('.hbs', exhbs({helpers:{formatDate , stripTags,truncate , editIcon, select
 }, defaultLayout: 'main', extname: ".hbs" }))
 app.set('view engine', '.hbs')
 
@@ -51,6 +67,12 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 
+
+// Set global var
+app.use(function(req,res,next){
+    res.locals.user = req.user || null 
+    next()
+})
 // Static folder 
 app.use(express.static(path.join(__dirname, 'public')))
 
--- routes/stories.js
+++ routes/stories.js
@@ -36,6 +36,93 @@ router.get('/',ensureAuth, async (req,res)=>{
    }
 })
 
+// Get one story
+router.get('/:id',ensureAuth, async (req,res)=>{
+   try {
+       let story =  await Story.findById(req.params.id)
+                     .populate('user')
+                     .lean()
+     if (!story) {
+       return res.render('error/404')
+    } 
+       res.render('stories/show',{story})
+   } catch (error) {
+       console.log(error)
+       res.render('error/404')
+   }
+})
 
+
+
+
+
+
+// show edit page
+router.get('/edit/:id',ensureAuth, async (req,res)=>{
+   const story = await Story.findOne({_id: req.params.id}).lean()
+if(!story){
+    return res.render('error/404')
+}
+if(story.user != req.user.id){
+    res.redirect('/stories')
+}else{
+    res.render('stories/edit',{story});
+}
+
+    
+})
+
+// Update story PUT /stories/:id
+
+router.put('/:id',ensureAuth,async (req,res)=>{
+try {
+
+let story = await Story.findById(req.params.id).lean()
+
+if(!story){
+    return res.render('error/404')
+}
+if(story.user != req.user.id){
+    res.redirect('/stories')
+}else{
+   story = await Story.findOneAndUpdate( {_id : req.params.id },req.body,{new:true , runValidators:true})
+   res.redirect('/stories')
+}
+} catch (error) {
+        console.error(error)
+        return res.render('error/500')
+    
+    
+
+
+
+}})
+
+// DELETE Story
+//route DELETE /stories/
+
+
+router.delete('/:id',ensureAuth, async(req,res)=>{
+    try {
+        await Story.remove({_id:req.params.id})
+        res.redirect('/dashboard')
+    } catch (error) {
+        console.error(error)
+        return res.render('error/500')
+    }
+    
+})
+
+
+
+router.get('/user/:userId',ensureAuth,async (req,res)=>{
+   try {
+       const stories = await Story.find({user : req.params.userId , status:'public'}).populate('user').lean()
+       res.render('stories/index',{stories})
+   } catch (error) {
+       console.error(error)
+       res.render('error/500')
+   }
+})
 
 module.exports = router ;
